e7577ac877df2fc89fa6b00e6c3cd6877ec9e673
JamesJoker <49804752+JamesJoker@users.noreply.github.com>
2025-02-08 10:38:24 +0800

    Add fetch Encapsulation class

---
+++ vue-project/src/Models/apiRequest.ts
@@ -0,0 +1,50 @@
+class apiRequest {
+
+    get(url: string, headers: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            fetch(url,{
+                headers: headers
+            })
+            .then((response) => {resolve(response)})
+            .catch((error) => {reject(error)})
+        })
+    }
+
+    post(url: string, data: string, headers: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                body: data,
+                headers: headers,
+                method: "POST"
+            })
+            .then((response) => {resolve(response)})
+            .catch((error) => {reject(error)})
+        })
+    }
+
+    put(url: string, data: string, headers: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                body: data,
+                headers: headers,
+                method: "PUT"
+            })
+            .then((response) => {resolve(response)})
+            .catch((error) => {reject(error)})
+        })
+    }
+
+    delete(url: string, data: string, headers: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                headers: headers,
+                body: data,
+                method: "DELETE"
+            })
+            .then((response) => {resolve(response)})
+            .catch((error) => {reject(error)})
+        })
+    }
+}
+
+export default new apiRequest();
