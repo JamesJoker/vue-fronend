5ad251c6e11e728f5fafab42436fdf7d14941537
JamesJoker <49804752+JamesJoker@users.noreply.github.com>
2025-02-08 10:38:47 +0800

    Add work class

---
+++ vue-project/src/Models/Work.ts
@@ -0,0 +1,39 @@
+import apiRequest from "./apiRequest";
+import APIPath from "./BackendAPI/api_path";
+
+class Work {
+    id: number | undefined;
+    name: string | undefined;
+    ownergroup: string[] | undefined;
+    frequency: string | undefined;
+
+    init(id: number, name: string, ownergroup: string[], frequency: string) {
+        this.id = id,
+        this.name = name;
+        this.ownergroup = ownergroup;
+        this.frequency = frequency;
+    }
+
+    create(work: Work) {
+        let headers = {
+            'content-type': 'application/json'
+        }
+        return apiRequest.post(APIPath.HouseWork(), JSON.stringify(work), headers);
+    }
+
+    modify(work: Work) {
+        let headers = {
+            'content-type': 'application/json'
+        }
+        return apiRequest.put(APIPath.HouseWork(), JSON.stringify(work), headers);
+    }
+
+    delete(work: Work) {
+        let headers = {
+            'content-type': 'application/json'
+        }
+        return apiRequest.delete(APIPath.HouseWork(), JSON.stringify(work), headers);
+    }
+}
+
+export default Work;
